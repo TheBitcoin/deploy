fc7a0ae595f4b7f542ea00042d8bca9fa688629c
TheBitcoin <tutti.eescalicha@gmail.com>
2016-09-28 10:56:16 +0200

    before device installation

---
+++ config/initializers/bitfinex.rb
@@ -0,0 +1,6 @@
+require 'bitfinex'
+
+Bitfinex::Client.configure do |conf|
+  conf.secret = 'beyJI6D1edWLum0EmtmUMkbe7ezIUUI1iYtbe7brLKC'
+  conf.api_key = 'AIwyFDIOxLQRE15QyPRGKGnTFxepuH9FQRkgatI3FaI'
+end
