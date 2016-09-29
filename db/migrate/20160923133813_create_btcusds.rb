a6f2e430d852d1c7f2d2e3074e91e6165f5f15a6
TheBitcoin <tutti.eescalicha@gmail.com>
2016-09-29 14:26:49 +0200

    add migration add column balance

--- db/migrate/20160923133813_create_btcusds.rb
+++ db/migrate/20160923133813_create_btcusds.rb
@@ -1,14 +1,15 @@
 class CreateBtcusds < ActiveRecord::Migration[5.0]
   def change
-    create_table: btcusds do |t |
-      t.float: high t.float: last
-      t.float: bid
-      t.integer: timestamp
-      t.float: vwap
-      t.float: volume
-      t.float: low
-      t.float: ask
-      t.float: open
+    create_table :btcusds do |t |
+      t.float :high
+      t.float :last
+      t.float :bid
+      t.integer :timestamp
+      t.float :vwap
+      t.float :volume
+      t.float :low
+      t.float :ask
+      t.float :open
       t.timestamps
     end
   end
